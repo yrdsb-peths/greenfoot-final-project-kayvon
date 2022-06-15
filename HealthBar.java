56c25202b9a4fd50cacc5ef4031fb85a63eba973
KayvonN <102381370+KayvonN@users.noreply.github.com>
2022-06-15 13:32:30 -0400

    Add files via upload

--- HealthBar.java
+++ HealthBar.java
@@ -20,11 +20,16 @@ public class HealthBar extends Actor
     {
         update();
     }
+
     public void act()
     {
         update();
         youLose();
     }
+    /**
+     * health bar colo is set and is positioned next to score
+     */
+
     public void update()
     {
         setImage(new GreenfootImage(healthBarWidth, healthBarHeight));
@@ -34,16 +39,22 @@ public class HealthBar extends Actor
         myImage.setColor(Color.RED);
         myImage.fillRect( 1, 1, health*pixelsPerHealthPoint, healthBarHeight);
     }
+
     public void loseHealth()
     {
         health--;
     }
+    /**
+     * when health is 0, game over screen will play
+     * game is over
+     */
+
     public void youLose()
     {
         if (health == 0)
         {
-            getWorld().addObject(new YouLose(), 300, 300);
-            Greenfoot.stop();
+            gameOverScreen gameOver = new gameOverScreen();
+            Greenfoot.setWorld(gameOver);
         }
     }
 }
